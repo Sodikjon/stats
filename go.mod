a5a56553d0b84aaf320a464374b2d3887ccd98ff
Sodiqjon Hamroev <hamroev.sodikjon@gmail.com>
2020-09-27 19:10:14 +0500

    добавленны файлы расчета статистики

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Sodikjon/stats
 
 go 1.15
+
+require github.com/Sodikjon/bank v0.0.0-20200927123234-72f48a2e364f // indirect
---
+++ pkg/stats/example_test.go
@@ -0,0 +1,75 @@
+package stats
+
+import (
+	"fmt"
+
+	"github.com/Sodikjon/bank/pkg/bank/types"
+)
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+			ID:       2,
+			Amount:   5_00,
+			Category: "Cat1",
+		},
+		{
+			ID:       1,
+			Amount:   8_00,
+			Category: "Cat1",
+		},
+		{
+			ID:       3,
+			Amount:   7_00,
+			Category: "Cat2",
+		},
+		{
+			ID:       4,
+			Amount:   9_00,
+			Category: "Cat2",
+		},
+		{
+			ID:       5,
+			Amount:   -3_00,
+			Category: "Cat3",
+		},
+	}
+
+	fmt.Println(Avg(payments))
+
+	//Output: 520
+}
+
+func ExampleTotalInCategory() {
+	payments := []types.Payment{
+		{
+			ID:       2,
+			Amount:   5_00,
+			Category: "Cat1",
+		},
+		{
+			ID:       1,
+			Amount:   8_00,
+			Category: "Cat1",
+		},
+		{
+			ID:       3,
+			Amount:   7_00,
+			Category: "Cat2",
+		},
+		{
+			ID:       4,
+			Amount:   9_00,
+			Category: "Cat2",
+		},
+		{
+			ID:       5,
+			Amount:   -3_00,
+			Category: "Cat3",
+		},
+	}
+
+	fmt.Println(TotalInCategory(payments, "Cat2"))
+
+	//Output: 1600
+}
